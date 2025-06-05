02e34cc6383426c782c81c3e6a5cc53463038d52
manish <manish342900@gmail.com>
2025-06-05 23:13:27 +0530

    path invalid fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ if( process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(_dirname,"../frontend/build")))
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(_dirname,"..frontend","build","index.html"))
+        res.sendFile(path.join(_dirname,"../frontend","build","index.html"))
     })
 }
 app.use(express.json({ limit: '5mb' }));
